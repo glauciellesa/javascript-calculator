1c2505d82ff8b32247eaf5e231beee31ef8b4161
Glaucielle Sá <glauciellesa@gmail.com>
2022-05-29 16:07:15 +0200

    Add calculate logic and bugfixes

--- calculator.js
+++ calculator.js
@@ -1,19 +1,35 @@
 // Selector
 let output = document.querySelector(".output");
 let result = document.querySelector(".result");
-let buttons = document.querySelectorAll("button");
+let keys = document.querySelectorAll("button");
 
-function ouvir(button){
-    button.addEventListener("click", calculate);
-} 
 
 // Eventlistener 
-buttons.forEach(ouvir);
 
-buttons.forEach(function ouvir(button){
+keys.forEach(button => {
     button.addEventListener("click", calculate);
 });
 
 function calculate() {
-    
+    let buttonText = this.innerText;
+    if (buttonText === "AC") {
+        output.innerText = " ";
+        result.innerText = 0;
+        return
+    }
+    if (buttonText === "DEL") {
+        output.textContent = output.textContent.substr(0, output.textContent.length-1);
+        return;
+    }
+    if (buttonText === "=") {
+        result.innerText = eval(output.innerText);
+        result.style.animation = "big 0.5s ease-in-out";
+        output.style.animation = "big 0.5s ease-in-out";
+        result.style.animation = "forwards";
+        output.style.animation = "forwards";
+    }
+    else {
+        output.textContent += buttonText;
+        return; 
+    }
 }
